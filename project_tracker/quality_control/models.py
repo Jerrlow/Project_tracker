6c5f847ae903c22e6d25bfe593a35de934175de9
Jerllow <nastya.swail@gmail.com>
2024-05-01 13:24:54 +0300

    Изменение quality_control.models

--- project_tracker/quality_control/models.py
+++ project_tracker/quality_control/models.py
@@ -3,12 +3,13 @@ from tasks.models import Project, Task
 
 
 class BugReport(models.Model):
+    STATUS_CHOICES = [('New', 'Новая'), ('In_progress', 'В работе'), ('Completed', 'Завершена')]
     title = models.CharField(max_length=255)
     description = models.TextField()
     project = models.ForeignKey(Project, on_delete=models.CASCADE)
     task = models.ForeignKey(Task, on_delete=models.SET_NULL, null=True)
-    status = models.CharField(max_length=50)
-    priority = models.IntegerField()
+    status = models.CharField(max_length=50, choices=STATUS_CHOICES, default='New')
+    priority = models.IntegerField(choices=((1, 'Low'), (2, 'Medium'), (3, 'High'), (4, 'Critical'), (5, 'Blocker')))
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
@@ -21,8 +22,13 @@ class FeatureRequest(models.Model):
     description = models.TextField()
     project = models.ForeignKey(Project, on_delete=models.CASCADE)
     task = models.ForeignKey(Task, on_delete=models.SET_NULL, null=True)
-    status = models.CharField(max_length=50)
-    priority = models.IntegerField()
+    STATUS_CHOICES = (
+        ('pending', 'В рассмотрении'),
+        ('approved', 'Принято'),
+        ('rejected', 'Отклонено'),
+    )
+    status = models.CharField(max_length=20, choices=STATUS_CHOICES, default='pending')
+    priority = models.IntegerField(choices=((1, 'Low'), (2, 'Medium'), (3, 'High'), (4, 'Critical')))
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
