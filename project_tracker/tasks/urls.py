cdb68fb7d1ab4a4b600f009b1db2f28705720fee
Jerllow <nastya.swail@gmail.com>
2024-05-01 23:13:58 +0300

    Диспетчер URL (Добавили список проектов#2)

--- project_tracker/tasks/urls.py
+++ project_tracker/tasks/urls.py
@@ -1,12 +1,15 @@
 from django.urls import path, include
 from tasks import views
 
+
 app_name = 'tasks'
 
 urlpatterns = [
     path('', views.index),
     path('quality_control/', include('quality_control.urls')),
-    path('project_list/', views.project_list, name='project_list'),
+    path('projects/', views.projects_list, name='projects_list'),
+    path('projects/<int:project_id>/', views.project_detail, name='project_detail'),
+    path('projects/<int:project_id>/tasks/<int:task_id>/', views.task_detail, name='task_detail'),
 ]
 
 
--- project_tracker/tasks/views.py
+++ project_tracker/tasks/views.py
@@ -1,6 +1,7 @@
 from django.urls import reverse
 from django.http import HttpResponse
 from .models import Project, Task
+from django.shortcuts import get_object_or_404
 
 
 def index(request):
@@ -12,10 +13,27 @@ def index(request):
     return HttpResponse(f'<h1>Главная страница приложения Tasks</h1><p>{link_html}</p>')
 
 
-def project_list(request):
+def projects_list(request):
     projects = Project.objects.all()
-    projects_html = '<h1>Список всех проектов</h1><ul>'
+    projects_html = '<h1>Список проектов</h1><ul>'
     for project in projects:
-        projects_html += f"<li>{project.name}</li>"
-    projects_html += "</ul>"
+        projects_html += f'<li><a href="{project.id}/">{project.name}</a></li>'
+    projects_html += '</ul>'
     return HttpResponse(projects_html)
+
+
+def project_detail(request, project_id):
+    project = get_object_or_404(Project, id=project_id)
+    tasks = project.tasks.all()
+    response_html = f'<h1>{project.name}</h1><p>{project.description}</p>'
+    response_html += '<h2>Задачи</h2><ul>'
+    for task in tasks:
+        response_html += f'<li><a href="tasks/{task.id}/">{task.name}</a></li>'
+    response_html += '</ul>'
+    return HttpResponse(response_html)
+
+def task_detail(request, project_id, task_id):
+    project = get_object_or_404(Project, id=project_id)
+    task = get_object_or_404(Task, id=task_id, project=project)
+    response_html = f'<h1>{task.name}</h1><p>{task.description}</p>'
+    return HttpResponse(response_html)
