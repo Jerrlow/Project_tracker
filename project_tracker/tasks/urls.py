f829e72ac057f902fbea87d7bfbc7a57f67a4daf
Jerllow <nastya.swail@gmail.com>
2024-05-02 12:25:42 +0300

    Шаблоны (Перемещение кнопки "Перейти в систему контроля качества" в навигационное меню)

--- project_tracker/tasks/urls.py
+++ project_tracker/tasks/urls.py
@@ -11,7 +11,6 @@ urlpatterns = [
     path('projects/<int:project_id>/', views.project_detail, name='project_detail'),
     path('projects/<int:project_id>/tasks/<int:task_id>/', views.task_detail, name='task_detail'),
     #path('index/', views.index, name='index'),
-
 ]
 
 
