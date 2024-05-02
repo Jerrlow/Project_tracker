5733d4984ec5007cac454d2bd4222fe19c896519
Jerllow <nastya.swail@gmail.com>
2024-05-02 22:18:47 +0300

    CRUD для quality_control

--- project_tracker/quality_control/urls.py
+++ project_tracker/quality_control/urls.py
@@ -13,4 +13,10 @@ urlpatterns = [
     path('features/<int:feature_id>/', feature_detail, name='feature_detail'),  # Маршрут для получения деталей конкретного улучшения
     path('bugs/create/', views.bug_report_create, name='bug_report_create'),
     path('features/create/', views.feature_request_create, name='feature_request_create'),
+
+    path('bugs/<int:bug_id>/update/', views.bug_report_update, name='bug_update'),
+    path('bugs/<int:bug_id>/delete/', views.bug_report_delete, name='bug_delete'),
+    path('features/<int:feature_id>/update/', views.feature_request_update, name='feature_request_update'),
+    path('features/<int:feature_id>/delete/', views.feature_request_delete, name='feature_request_delete'),
+
 ]
--- project_tracker/quality_control/views.py
+++ project_tracker/quality_control/views.py
@@ -20,50 +20,11 @@ def index(request):
     return render(request, 'quality_control/index.html')
 
 
-# def bug_list(request):
-#     bugs = BugReport.objects.all()
-#     bugs_html = "<h1>Список отчетов об ошибках</h1><ul>"
-#     for bug in bugs:
-#         bugs_html += f'<li><a href="{bug.id}/">{bug.title}</a></li>'
-#     bugs_html += '</ul>'
-#     return HttpResponse(bugs_html)
-
 
 def bug_list(request):
     bugs = BugReport.objects.all()
     return render(request, 'quality_control/bug_list.html', {'bugs': bugs})
 
-
-# def feature_list(request):
-#     feature_requests = FeatureRequest.objects.all()
-#     feature_requests_html = "<h1>Список запросов на улучшение</h1><ul>"
-#     for feature_request in feature_requests:
-#         feature_requests_html += f'<li><a href="{feature_request.id}/">{feature_request.title}</a></li>'
-#     feature_requests_html += '</ul>'
-#     return HttpResponse(feature_requests_html)
-
-def feature_list(request):
-    feature_requests = FeatureRequest.objects.all()
-    return render(request, 'quality_control/feature_list.html', {'features': feature_requests})
-
-# def bug_detail(request, bug_id):
-#     bug = get_object_or_404(BugReport, id=bug_id)
-#
-#     project_url = reverse('tasks:project_detail', args=[bug.project.id])
-#     task_url = reverse('tasks:task_detail', args=[bug.project.id, bug.task.id])
-#
-#     response_html = f"<h1>Детали бага {bug.title}</h1>"
-#     response_html += f"<p>Статус бага: <span style='color: red;'>{bug.status}</span></p>"
-#     response_html += f"<p>Дата возникновения бага: <span style='color: red;'>{bug.created_at}</span></p>"
-#     response_html += f"<p><strong>Подробнее:</strong></p>"
-#
-#     response_html += f"<p>Проект: <a href='{project_url}'>{bug.project.name}</a></p>"
-#     response_html += f"<p>Описание проекта: {bug.project.description}</p>"
-#     response_html += f"<p>Задача: <a href='{task_url}'>{bug.task.name}</a></p>"
-#     response_html += f"<p>Описание задачи: {bug.task.description}</p>"
-#     response_html += f"<p>Статус задачи: {bug.task.status}</p>"
-#
-#     return HttpResponse(response_html)
 
 def bug_detail(request, bug_id):
     bug = get_object_or_404(BugReport, id=bug_id)
@@ -79,21 +40,10 @@ def bug_detail(request, bug_id):
 
     return render(request, 'quality_control/bug_detail.html', context)
 
-# def feature_detail(request, feature_id):
-#     feature_request = get_object_or_404(FeatureRequest, id=feature_id)
-#     project_url = reverse('tasks:project_detail', args=[feature_request.project.id])
-#     task_url = reverse('tasks:task_detail', args=[feature_request.project.id, feature_request.task.id])
-#     response_html = f"<h1>Детали улучшения {feature_request.title}</h1>"
-#     response_html += f"<p>Статус: <span style='color: red;'>{feature_request.status}</span></p>"
-#     response_html += f"<p>Приоритет: <span style='color: red;'>{feature_request.priority}</span></p>"
-#     response_html += f"<p><strong>Подробнее:</strong></p>"
-#     response_html += f"<p>Проект: <a href='{project_url}'>{feature_request.project.name}</a></p>"
-#     response_html += f"<p>Описание проекта: {feature_request.project.description}</p>"
-#     response_html += f"<p>Задача: <a href='{task_url}'>{feature_request.task.name}</a></p>"
-#     response_html += f"<p>Описание задачи: {feature_request.task.description}</p>"
-#     response_html += f"<p>Статус задачи: {feature_request.task.status}</p>"
-#
-#     return HttpResponse(response_html)
+
+def feature_list(request):
+    feature_requests = FeatureRequest.objects.all()
+    return render(request, 'quality_control/feature_list.html', {'features': feature_requests})
 
 def feature_detail(request, feature_id):
     feature_request = get_object_or_404(FeatureRequest, id=feature_id)
@@ -136,3 +86,47 @@ def feature_request_create(request):
     else:
         form = FeatureRequestForm()
     return render(request, 'quality_control/feature_request_form.html', {'form': form})
+
+
+
+# Обновление бага
+def bug_report_update(request, bug_id):
+    bug = get_object_or_404(BugReport, id=bug_id)
+    if request.method == 'POST':
+        form = BugReportForm(request.POST, instance=bug)
+        if form.is_valid():
+            form.save()
+            return redirect('quality_control:bug_detail', bug_id=bug.id)
+    else:
+        form = BugReportForm(instance=bug)
+    return render(request, 'quality_control/bug_report_form_update.html', {'form': form})
+
+# Удаление бага
+def bug_report_delete(request, bug_id):
+
+    # project = get_object_or_404(Project, pk=project_id)
+    # project.delete()
+    # return redirect('tasks:projects_list')
+    bug = get_object_or_404(BugReport, id=bug_id)
+    bug.delete()
+    return redirect('quality_control:bug_list')
+
+
+def feature_request_update(request, feature_id):
+    feature_request = get_object_or_404(FeatureRequest, id=feature_id)
+    if request.method == 'POST':
+        form = FeatureRequestForm(request.POST, instance=feature_request)
+        if form.is_valid():
+            form.save()
+            return redirect('quality_control:feature_detail', feature_id=feature_request.id)
+    else:
+        form = FeatureRequestForm(instance=feature_request)
+    return render(request, 'quality_control/feature_request_form_update.html', {'form': form})
+
+
+def feature_request_delete(request, feature_id):
+    feature_request = get_object_or_404(FeatureRequest, id=feature_id)
+    feature_request.delete()
+    return redirect('quality_control:feature_list')
+    # return render(request, 'quality_control/feature_request_form_delete.html', {'feature_request': feature_request})
+
